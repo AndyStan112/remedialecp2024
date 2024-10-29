a41eba516356e16e24a7ff6ecb3ed226f0e95917
AndyStan112 <stanica.andrei28@gmail.com>
2024-10-29 21:55:43 +0200

    first week

--- week5/hello.c
+++ week5/hello.c
@@ -9,7 +9,7 @@ int char_to_number(char c)
     }
     if (toupper(c) < 'A' || toupper(c) > 'F')
     {
-        printf("Invalid character\n");
+        printf("Invalid character");
         exit(1);
     }
     return toupper(c) - 'A' + 10;
@@ -22,14 +22,87 @@ int read_number(int b)
     while ((c = getchar()) != EOF)
     {
         n = n * b + char_to_number(c);
-        ;
     }
     return n;
+}
+
+void collapse_spaces()
+{
+    char c;
+    int n = 0;
+    while (isspace((c = getchar())))
+    {
+    }
+    ungetc(c, stdin);
+}
+
+void read_paragraph(char delimiter)
+{
+    char c;
+    while ((c = getchar()) != EOF)
+    {
+        if (isspace(c))
+        {
+            collapse_spaces();
+            putchar(delimiter);
+        }
+        else
+        {
+            putchar(c);
+        }
+    }
+}
+int peek()
+{
+    int c = getchar();
+    ungetc(c, stdin);
+    return c;
+}
+void ignore_comments()
+{
+    // naive, doesn't check for nesting unclosed comments
+    int c;
+    int show = 1;
+    while ((c = getchar()) != EOF)
+    {
+
+        if (c == '/')
+        {
+
+            if ((c = getchar()) == '*')
+            {
+                show = 0;
+                continue;
+            }
+
+            else
+            {
+                if (show)
+                {
+                    putchar('/');
+                }
+            }
+        }
+
+        if (c == '*')
+        {
+            if ((c = getchar()) == '/')
+            {
+                show = 1;
+                continue;
+            }
+        }
+
+        if (show)
+            putchar(c);
+    }
+
+    putchar('\n');
 }
 
 int main()
 {
 
-    printf("%d\n", read_number(16));
+    ignore_comments();
     return 0;
 }
